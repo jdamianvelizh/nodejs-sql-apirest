7a778d0690ee8737a6de50f2735b419f020f2361
DamianVH1 <josephveliz99@hotmail.com>
2023-07-31 10:25:12 -0600

    Creacion de endpoints iniciales

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ const app = express()
 
 app.listen(3000)
 console.log("server listening on 3000");
+
+app.get('/empleados', (req, res) => res.send('Obteniendo empleados'))
+app.post('/empleados', (req, res) => res.send('Creando empleados'))
+app.put('/empleados', (req, res) => res.send('Actualizando empleados'))
+app.delete('/empleados', (req, res) => res.send('Eliminando empleados'))
